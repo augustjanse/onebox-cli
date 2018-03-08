b209ad44ed37535111502d27fe233fec04d16d60
August Janse <augustj@kth.se>
2018-03-08 20:06:25 +0100

    Clone MB repo

--- onebox-cli.rb
+++ onebox-cli.rb
@@ -1,13 +1,19 @@
 require "onebox"
 
-require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_artist_onebox'
-require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_event_onebox'
-require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_label_onebox'
-require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_place_onebox'
-require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_recording_onebox'
-require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_release_onebox'
-require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_releasegroup_onebox'
-require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_work_onebox'
+2.times do
+  begin
+    require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_artist_onebox'
+    require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_event_onebox'
+    require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_label_onebox'
+    require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_place_onebox'
+    require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_recording_onebox'
+    require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_release_onebox'
+    require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_releasegroup_onebox'
+    require_relative 'discourse-musicbrainz-onebox/engine/musicbrainz_work_onebox'
+  rescue LoadError
+    system('git clone https://github.com/phw/discourse-musicbrainz-onebox.git')
+  end
+end
 
 Onebox.options.load_paths.push(File.join(File.dirname(__FILE__), "discourse-musicbrainz-onebox/templates"))
 url = "https://musicbrainz.org/artist/79ba6a62-037c-4f16-bbf3-8fe8b363451b"
